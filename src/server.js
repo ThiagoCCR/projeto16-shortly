fd8f59ade1a1619f9978398da608f34440c8ebfb
ThiagoCCR <thiago.ccr96@gmail.com>
2022-10-11 16:06:08 -0300

    feat test get

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get((req,res)=>{
+    res.send("oi")
+})
 
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
